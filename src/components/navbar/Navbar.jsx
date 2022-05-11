4e7b7618f2cc5247c06bc1610c7fdfe5b45261dc
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2022-05-11 18:32:18 +0200

    authenticator added to entire app

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,9 +8,21 @@ import {
   Logo,
 } from "./Styles";
 import logo from "../../assets/images/logo.png";
+import { Auth } from "aws-amplify";
+import { useNavigate } from "react-router";
+import * as ROUTES from "../../constants/routes";
 
 export default function Navbar() {
-  const handleSignout = () => {};
+  const navigate = useNavigate();
+
+  const handleSignout = async () => {
+    try {
+      await Auth.signOut();
+      navigate(ROUTES.LOGIN);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <StyledNavbar>
